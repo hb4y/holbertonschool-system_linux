96d20297e400d77c71b135e81f0e332a59479b41
hb4y <hugobaymon@gmail.com>
2020-07-28 01:57:57 +0000

    read until EOF

--- 0x01-getline/_getline.c
+++ 0x01-getline/_getline.c
@@ -68,33 +68,30 @@ char *_getline(const int fd)
 {
 	static line_n *head;
 	char buf[READ_SIZE], *line;
-	int n, full, i, j, end;
+	int n, full, i, j;
 
 	line = NULL;
 	full = 0;
 
+	memset(buf, '\0', READ_SIZE);
 	n = read(fd, buf, READ_SIZE);
 
-	for (end = 0; n <= READ_SIZE && !end && !head; n = read(fd, buf, READ_SIZE))
+	for (; n > 0 && !head; n = read(fd, buf, READ_SIZE))
 	{
 		full += n;
 		for (i = j = 0; j < READ_SIZE; j++)
 		{
-			if (buf[j] == '\n' || buf[j] == EOF)
+			if (buf[j] == '\n' || j == full)
 			{
 				line = malloc((j - i) * sizeof(char));
 				if (!line)
 					return (NULL);
+				memset(line, '\0', (j - i));
 				strncpy(line, &buf[i], (j - i));
 				line[(j - i)] = '\0';
 				create_node(&head, fd, line);
-				if (buf[j] != EOF)
-					i = j + 1;
-				else
-				{
-					end = 1;
-					break;
-				}
+				printf("::[%s]::", line);
+				i = j + 1;
 			}
 		}
 	}
