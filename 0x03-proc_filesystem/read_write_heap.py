2dd2cdc4209d579ccab795fcbc2cf5b446e724fc
hb4y <hugobaymon@gmail.com>
2020-08-27 01:48:49 +0000

    __name__ == __main__

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -2,50 +2,55 @@
 """
 Write in the heap of a current process
 """
-
 import sys
 
-if len(sys.argv) != 4:
-    print("Usage: read_write_heap.py <pid> <search_string> <replace_string>")
-    exit(1)
 
-pid = int(sys.argv[1])
-search_string = str(sys.argv[2])
-replace_string = str(sys.argv[3])
+def write_heap():
+    if len(sys.argv) != 4:
+        e = "Usage: read_write_heap.py <pid> <search_string> <replace_string>"
+        print(e)
+        exit(1)
 
-if (pid <= 0) or (search_string == "") or (replace_string == ""):
-    print("Error: bad argument format")
-    sys.exit(1)
+    pid = int(sys.argv[1])
+    search_string = str(sys.argv[2])
+    replace_string = str(sys.argv[3])
 
-try:
-    men_map = open('/proc/{}/maps'.format(pid), 'r')
-except:
-    print("Error: cannot open map")
-    exit(1)
+    if (pid <= 0) or (search_string == "") or (replace_string == ""):
+        print("Error: bad argument format")
+        sys.exit(1)
 
-try:
-    mem_file = open("/proc/{}/mem".format(pid), 'rb+')
-except:
-    print("Error: Can not open men file")
-    men_map.close()
-    exit(1)
+    try:
+        men_map = open('/proc/{}/maps'.format(pid), 'r')
+    except:
+        print("Error: cannot open map")
+        exit(1)
 
-for line in men_map.readlines():
-    info = line.split()
-    if "[heap]" in info:
-        addr = info[0].split("-")
-        addr_start = int(addr[0], 16)
-        addr_end = int(addr[1], 16)
-        mem_file.seek(addr_start)
-        heap = mem_file.read(addr_end - addr_start)
-        i = heap.find(str.encode(search_string))
+    try:
+        mem_file = open("/proc/{}/mem".format(pid), 'rb+')
+    except:
+        print("Error: Can not open men file")
+        men_map.close()
+        exit(1)
 
-        if i == -1:
+    for line in men_map.readlines():
+        info = line.split()
+        if "[heap]" in info:
+            addr = info[0].split("-")
+            addr_start = int(addr[0], 16)
+            addr_end = int(addr[1], 16)
+            mem_file.seek(addr_start)
+            heap = mem_file.read(addr_end - addr_start)
+            i = heap.find(str.encode(search_string))
+
+            if i == -1:
+                break
+
+            mem_file.seek(addr_start + i)
+            mem_file.write(str.encode(sys.argv[3]) + b'\x00')
             break
 
-        mem_file.seek(addr_start + i)
-        mem_file.write(str.encode(sys.argv[3]) + b'\x00')
-        break
+    men_map.close()
+    mem_file.close()
 
-men_map.close()
-mem_file.close()
+if __name__ == '__main__':
+    write_heap()
