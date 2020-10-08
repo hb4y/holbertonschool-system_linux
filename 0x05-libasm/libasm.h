1b3fab2a6f06e7c113623385394fbd7636c17a09
hb4y <hugobaymon@gmail.com>
2020-10-08 23:54:16 +0000

    update header

--- 0x05-libasm/libasm.h
+++ 0x05-libasm/libasm.h
@@ -1,8 +1,11 @@
+#ifndef LIB_ASM_H
+#define LIB_ASM_H
 
-#ifndef LIBASM_H
-#define LIBASM_H
+int asm_strcmp(const char *s1, const char *s2);
+int asm_strncmp(const char *s1, const char *s2, size_t n);
+char *asm_strchr(const char *s, int c);
+char *asm_strstr(const char *haystack, const char *needle);
+void *asm_memcpy(void *dest, const void *src, size_t n);
+size_t asm_putc(int c);
 
-
-size_t asm_strlen(const char *str);
-
-#endif
+#endif /* LIB_ASM_H */
