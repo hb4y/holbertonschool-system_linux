df70542e715de91a06be94a7e888b9c3c8c5396b
hb4y <hugobaymon@gmail.com>
2021-08-05 22:03:35 -0500

    [Basic info] of python list

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -10,9 +10,9 @@ void print_python_list(PyObject *p)
 	Py_ssize_t i;
 
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %lu\n", list_size);
+	printf("[*] Size of the Python List = %lu\n", size);
 	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 
-	for (i = 0; i < list_size; i++)
+	for (i = 0; i < size; i++)
 		printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 }
