082eb5d2d0ab5d0f43981c1242632c10309c5090
hb4y <hugobaymon@gmail.com>
2021-08-05 23:14:49 -0500

    pleaseeeee cheeeeckkkkkkkk

--- 0x08_CPython/100-python.c
+++ 0x08_CPython/100-python.c
@@ -1,32 +1,45 @@
 #include <Python.h>
-
 /**
- * print_python_int - Python integers for geeks
+ * print_python_int - Python int
  * @p: int
  */
 void print_python_int(PyObject *p)
 {
-	Py_ssize_t size;
-	Py_ssize_t i;
-	digit *digit;
-	unsigned long val = 0;
+	PyLongObject *_b, *_d;
+	Py_ssize_t s, i, j;
+	digit *_p, u, *c;
+	twodigits z;
+	int tmp_dig;
 
 	if (!p || !PyLong_Check(p))
 	{
 		printf("Invalid Int Object\n");
 		return;
 	}
-	size = ((PyVarObject *)(p))->ob_size;
-	digit = ((PyLongObject *)(p))->ob_digit;
-	if (abs(size) > 3 || (abs(size) == 3 && digit[2] > 15))
-	{
-		printf("C unsigned long int overflow\n");
+	_d = (PyLongObject *)p;
+	tmp_dig = (33 * _PyLong_DECIMAL_SHIFT) /
+		(10 * PyLong_SHIFT - 33 * _PyLong_DECIMAL_SHIFT);
+	s = 1 + abs(Py_SIZE(_d)) + abs(Py_SIZE(_d)) / tmp_dig;
+	_b = _PyLong_New(s);
+	if (_b == NULL)
 		return;
-	}
-	for (i = 0; i < abs(size); i++)
-		val += digit[i] * (1L << (i * PyLong_SHIFT));
-	if (size < 0)
+	_p = _d->ob_digit, c = _b->ob_digit,  s = 0;
 
-	putchar('-');
-	printf("%lu\n", val);
+	for (i = abs(Py_SIZE(_d)); --i >= 0; )
+	{
+		u = _p[i];
+		for (j = 0; j < s; j++)
+		{
+			z = (twodigits)c[j] << PyLong_SHIFT | u;
+			u = (digit)(z / _PyLong_DECIMAL_BASE);
+			c[j] = (digit)(z - (twodigits)u * _PyLong_DECIMAL_BASE);
+		}
+		while (u)
+			c[s++] = u % _PyLong_DECIMAL_BASE, u /= _PyLong_DECIMAL_BASE;
+	}
+	s == 0 ? c[s++] = 0 : 0, Py_SIZE(_d) < 0 ? putchar('-') : 0;
+
+	for (i = s; --i >= 0;)
+		i == s - 1 ? printf("%u", c[i]) : printf("%09u", c[i]);
+	printf("\n");
 }
