ab624fc01ea62ef1ac539b3dd9e8d73046bda4bb
hb4y <hugobaymon@gmail.com>
2021-08-05 23:08:24 -0500

    python integers

---
+++ 0x08_CPython/100-python.c
@@ -0,0 +1,31 @@
+#include <Python.h>
+
+/**
+ * print_python_int - Python integers for geeks
+ * @p: int
+ */
+void print_python_int(PyObject *p)
+{
+	Py_ssize_t size;
+	Py_ssize_t i;
+	digit *digit;
+	unsigned long val = 0;
+
+	if (!p || !PyLong_Check(p))
+	{
+		printf("Invalid Int Object\n");
+		return;
+	}
+	size = ((PyVarObject *)(p))->ob_size;
+	digit = ((PyLongObject *)(p))->ob_digit;
+	if (abs(size) > 3 || (abs(size) == 3 && digit[2] > 15))
+	{
+		printf("C unsigned long int overflow\n");
+		return;
+	}
+	for (i = 0; i < abs(size); i++)
+		val += digit[i] * (1L << (i * PyLong_SHIFT));
+	if (size < 0)
+		putchar('-');
+	printf("%lu\n", val);
+}
