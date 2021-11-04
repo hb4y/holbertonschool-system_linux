2c1ffe11a15b84d25c9f4cb63f8a9418e3108228
hb4y <hugobaymon@gmail.com>
2021-11-03 21:56:57 -0500

    respond msg

---
+++ 0x0C-sockets/3-server.c
@@ -0,0 +1,56 @@
+#include "server.h"
+
+/**
+ * print_error - print error
+ * @s_error: error
+ */
+void print_error(char *s_error)
+{
+	printf("%s\n", s_error);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * main - listens to traffic on port 12345
+ * Return: 0 or error
+ */
+int main(void)
+{
+	int socket_a = 0, socket_c = 0;
+	int size = 0, c_size = 0;
+	int m_size = 0;
+	char c_msg[256] = {0};
+	struct sockaddr_in addr_server, addr_client;
+
+	socket_a = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (socket_a == -1)
+		print_error("socket failed");
+	addr_server.sin_family = AF_INET, addr_server.sin_port = htons(12345);
+	addr_server.sin_addr.s_addr = htonl(INADDR_ANY);
+	size = sizeof(addr_server);
+	if (bind(socket_a, (struct sockaddr *) &addr_server, size) == -1)
+		print_error("bind failed");
+	if (listen(socket_a, 10) == -1)
+		print_error("listen failed");
+	printf("Server listening on port %d\n", 12345);
+	c_size = sizeof(addr_client);
+	socket_c = accept(socket_a, (struct sockaddr *) &addr_client, (
+				(socklen_t *) &c_size));
+	if (socket_c == -1)
+		print_error("accept failed");
+	printf("Client connected: %s\n", inet_ntoa(addr_client.sin_addr));
+	printf("Message received: \"");
+	for (; ; memset(c_msg, '\0', 256))
+	{
+		m_size = recv(socket_c, c_msg, 256 - 1, 0);
+		if (m_size == -1)
+			print_error("recieve failed");
+		else if (m_size == 0)
+			break;
+		printf("%s", c_msg);
+	}
+	printf("\"\n");
+	close(socket_c), close(socket_a);
+
+	return (0);
+}
