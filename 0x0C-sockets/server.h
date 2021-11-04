287e1a41460ec17b906db7436bf949c7f9fa3c9b
hb4y <hugobaymon@gmail.com>
2021-11-03 21:04:18 -0500

    header

---
+++ 0x0C-sockets/server.h
@@ -0,0 +1,12 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+#endif
