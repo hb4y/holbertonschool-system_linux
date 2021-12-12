7aa15146d178feadaacab000ee15d5f3daba4217
hb4y <hugobaymon@gmail.com>
2021-12-11 20:22:39 -0500

    headers for operations

---
+++ 0x0C-sockets/ops.h
@@ -0,0 +1,12 @@
+#ifndef OPS
+#define OPS
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+#endif
